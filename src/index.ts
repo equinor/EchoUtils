0260e984868952b9938bfabc3b5d3ddd5a5a5898
jorgenholme96 <jorgen.holme@bouvet.no>
2021-03-19 14:15:33 +0100

    Expose fireAndForget

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { useOnOutsideClick } from './hooks/useOnOutsideClick';
 export { useFocus } from './hooks/useFocus';
 export { arraysIsEqual } from './utils/arrayUtils';
 export { objectIsEmpty } from './utils/objectUtils';
+export { fireAndForget } from './utils/asyncUtils';
