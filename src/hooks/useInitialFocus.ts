359067b8a1d6d08a08c4aa0ef69f1f1ebe81f05b
Arne Kristian Jansen <9586805+aigelonic@users.noreply.github.com>
2021-04-07 11:13:48 +0200

    Adding test, removing obsolete
    Adding tests to the useWindowSize hook, ading the hook to index.ts
    to be exposed from utils.
    
    Removing useInitialFocus as it was nowhere in use.

--- src/hooks/useInitialFocus.ts
+++
@@ -1,9 +0,0 @@
-import { RefObject, useEffect } from 'react';
-const useInitialFocus = (ref: RefObject<HTMLInputElement>): void => {
-    useEffect(() => {
-        if (ref.current) {
-            ref.current.focus();
-        }
-    }, [ref]);
-};
-export default useInitialFocus;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 export { useOnOutsideClick } from './hooks/useOnOutsideClick';
 export { useFocus } from './hooks/useFocus';
+export { useWindowSize } from './hooks/useWindowSize';
 export { arraysIsEqual } from './utils/arrayUtils';
 export { objectIsEmpty, objectsIsEqual } from './utils/objectUtils';
 export { fireAndForget } from './utils/asyncUtils';
