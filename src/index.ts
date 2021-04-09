f4c4ddcf6c7d4fe481ed191aeee125d791990845
Hanne Kottum <HKOT@equinor.com>
2021-04-09 15:25:44 +0200

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,5 @@
  * Expose utils and hooks here
  */
 
-export { useOnOutsideClick } from './hooks/useOnOutsideClick';
-export { useFocus } from './hooks/useFocus';
-export { useDebounce } from './hooks/useDebounce';
-export { useWindowSize } from './hooks/useWindowSize';
-export { objectIsEmpty, objectsIsEqual, arraysIsEqual } from './utils/objectUtils';
-export { fireAndForget } from './utils/asyncUtils';
+export * from './hooks';
+export * from './types';
