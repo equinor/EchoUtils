3ed845724b3e8d8efdb1fcf799948d844d06f420
Hanne Kottum <HKOT@equinor.com>
2021-04-12 07:46:02 +0200

    added deeplink methods

---
+++ src/utils/deepLinkParams.ts
@@ -0,0 +1,24 @@
+export const availableSearchParams = ['instCode', 'tagNo', 'search'];
+
+type LinkParam = { [key: string]: string | null };
+
+export function getDeepLinkParams<T extends LinkParam>(
+    params: Array<keyof LinkParam> = availableSearchParams
+): Required<T> {
+    const queryParams = new URLSearchParams(new URL(window.location.href).search);
+    const linkParams = {} as LinkParam;
+    params.forEach((key) => {
+        linkParams[key] = queryParams.get(String(key));
+    });
+    return linkParams as Required<T>;
+}
+
+export const setDeepLinkParams = (param: string, value: string): void => {
+    const queryParams = new URLSearchParams(new URL(window.location.href).search);
+    if (value.length > 0) {
+        queryParams.set(param, value);
+    } else {
+        queryParams.delete(param);
+    }
+    window.history.replaceState({}, '', `${window.location.pathname}?${queryParams}`);
+};
