70388d4499330dfc3f160723a062215bf86ebdcf
Christopher Berge Hove <chris@nogglings.com>
2021-06-09 15:24:14 +0200

    bump versjon to v0.1.0 and doc

--- src/utils/fromatString.ts
+++ src/utils/fromatString.ts
@@ -1,7 +1,15 @@
-export function formatString( ...args: (string | undefined)[] ): string {
-    const strings = args.filter(arg  => arg !== undefined);
+/**
+ * Used for formatting string to echo standard.
+ * will handle undefined as input argument.
+ *
+ * @export
+ * @param {(...(string | undefined)[])} args
+ * @return {*}  {string}
+ */
+export function formatString(...args: (string | undefined)[]): string {
+    const strings = args.filter((arg) => arg !== undefined);
     if (strings.length > 0) {
-        return strings.join("-")
+        return strings.join('-');
     }
-    return "-"
+    return '-';
 }
