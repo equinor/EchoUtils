86b6409adb585598ea28eeeb1e8924eefe88a218
Noggling <christopher.hove@bouvet.no>
2021-06-10 12:06:36 +0000

    Deploying to docs from @ equinor/EchoUtils@6f1bdc140464edff9e0474690ab4a1786a7e58de 🚀

--- src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './useCleanup';
-export { useDebounce } from './useDebounce';
-export { useFocus } from './useFocus';
-export * from './useInitial';
-export { useOnOutsideClick } from './useOnOutsideClick';
-export { useWindowSize } from './useWindowSize';
