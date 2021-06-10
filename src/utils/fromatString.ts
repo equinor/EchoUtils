76242d0b1f1c5cbf103055e87ed8bfdc7b843356
Christopher Berge Hove <chris@nogglings.com>
2021-06-10 08:40:52 +0200

    removed flile

--- src/utils/fromatString.ts
+++
@@ -1,7 +0,0 @@
-export function formatString( ...args: (string | undefined)[] ): string {
-    const strings = args.filter(arg  => arg !== undefined);
-    if (strings.length > 0) {
-        return strings.join("-")
-    }
-    return "-"
-}
