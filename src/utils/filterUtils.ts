dfc3a3d7c99d15d5a8b5943e92b599533f67773e
jorgenholme96 <jorgen.holme@bouvet.no>
2021-06-18 10:13:45 +0200

    Make filter type safe

--- src/utils/filterUtils.ts
+++ src/utils/filterUtils.ts
@@ -1,6 +1,6 @@
-interface Filter {
-    [key: string]: string | number | boolean | Date;
-}
+type Filter<T> = {
+    [K in keyof T]: T[K];
+};
 
 /**
  * A function for filtering lists of objects.
@@ -12,7 +12,7 @@ interface Filter {
  * @param {Filter} propsToFilterOn
  * @return {*}  {T[]}
  */
-export function filterOnProps<T>(data: T[], propsToFilterOn: Filter): T[] {
+export function filterOnProps<T>(data: T[], propsToFilterOn: Filter<T>): T[] {
     const filters = Object.keys(propsToFilterOn);
     let filterData: T[] = data;
     for (const filter of filters) {
