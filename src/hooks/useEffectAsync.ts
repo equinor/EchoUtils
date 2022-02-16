e0eab40eaa76cb4d058788624d9ad9180f4489bd
Torbjørn Høiland <thoil@equinor.com>
2022-02-16 10:21:17 +0100

    Revert "removed empty file as this triggered tests that failed :("
    This reverts commit dcf265a8172aa102024d358ace07188e9a9db0bd.

---
+++ src/hooks/useEffectAsync.ts
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+type AsyncEffect = (signal: AbortSignal) => Promise<void | (() => void)> | void;
+
+/**
+ * Accepts a function that contains imperative, possibly effectful code.
+ * @export
+ * @param effect Imperative async function that can return a cleanup function. Also provides AbortSignal as input to handle cancellation of setstate/http requests
+ * @param deps If present, effect will only activate if the values in the list change.
+ */
+export function useEffectAsync(effect: AsyncEffect, deps: React.DependencyList = []): void {
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        const cleanupPromise = effect(abortController.signal);
+
+        return () => {
+            abortController.abort();
+
+            if (cleanupPromise instanceof Promise) {
+                cleanupPromise.then((cleanup) => cleanup && cleanup());
+            }
+        };
+        // Needs to be dynamic dependencies
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, deps);
+}
