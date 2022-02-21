bab8766304dc0b9fe5f500dac527a11e82f327f9
Csaba Kiss <ckiss@equinor.com>
2022-02-21 12:43:49 +0100

    Removed commented code

--- src/hooks/useIsSmallScreen.test.ts
+++ src/hooks/useIsSmallScreen.test.ts
@@ -1,18 +1,6 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { useIsSmallScreen } from './useIsSmallScreen';
 
-// it('should return true in case of small (w: 200) screen', () => {
-//     expect(isSmallScreen({ width: 200, height: 800 })).toBe(true);
-// });
-
-// it('should return true in case of small (w: 550) screen', () => {
-//     expect(isSmallScreen({ width: 200, height: 800 })).toBe(true);
-// });
-
-// it('should return false in case of large screen', () => {
-//     expect(isSmallScreen({ width: 560, height: 800 })).toBe(false);
-// });
-
 describe('useIsSmallScreen()', () => {
     afterEach(() => {
         Object.defineProperty(window, 'innerWidth', {
