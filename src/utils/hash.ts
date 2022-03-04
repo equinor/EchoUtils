24f7c6a10fb2f50bc318181a57e8013a6f1f4143
Erlend Andreas Hall <ehall@equinor.com>
2022-03-04 11:58:25 +0100

    added a hash helper with initial md5 support

---
+++ src/utils/hash.ts
@@ -0,0 +1,19 @@
+import md5 from "blueimp-md5";
+
+type HashMethod = "md5"
+
+/**
+ * Accepts a string value and hashes it with a given value.
+ * @param {string} type The hash method. Currently supports "md5".
+ * @param {string} value The value to be hashed.
+ */
+function createHash(type: HashMethod, value: string): string {
+    switch (type) {
+        case "md5":
+            return md5(value);
+        default:
+            throw new Error("Could not determine how to hash the value")
+    }
+}
+
+export { createHash };
