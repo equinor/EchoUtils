c1b04205d704a2bc4eda0bcf34c69b8d268c1e88
Erlend Andreas Hall <ehall@equinor.com>
2023-02-28 13:11:22 +0100

    moved DeviceInformation from framework

---
+++ src/utils/deviceInfo/deepDeviceInfo.ts
@@ -0,0 +1,175 @@
+import UAParser from 'ua-parser-js';
+import { DetailedDeviceInformation, SupportedPlatforms } from '../../types/device';
+
+interface DetailedDeviceInformationParserProps {
+    deviceInformation: DetailedDeviceInformation;
+    uaDataValues?: UADataValues;
+    uaParser: UAParser;
+}
+
+/**
+ * This object is concerned with gathering and holding information about the users device.
+ * This object will be read-only after initialization and should as a rule throw errors if the
+ * deviceInformation property is altered after intialization.
+ */
+class DetailedDeviceInformationProvider {
+    /** Represents the user agent string parser.
+     * This method is treated as a fallback if the User-Agent Client Hints API does not provide a coherent value
+     * or if the browser does not yet support it.
+     */
+    private readonly _uaParser: UAParser;
+
+    /**
+     * Stores the system report from a call to the underlying system.
+     * - If this is undefined, it is not yet initialized (it happens asynchronously).
+     */
+    private readonly _uaDataValues: UADataValues | undefined | null = undefined;
+
+    /** Holds the current device information and should be the single source of truth.
+     *
+     * It can be undefined because we have to asynchronously fetch underlying system information.
+     */
+    private readonly _deviceInformation: DetailedDeviceInformation | undefined = undefined;
+
+    private constructor(props: DetailedDeviceInformationParserProps) {
+        this._deviceInformation = props.deviceInformation;
+        this._uaDataValues = props.uaDataValues;
+        this._uaParser = props.uaParser;
+    }
+
+    public get deviceInformation(): DetailedDeviceInformation {
+        if (this._deviceInformation === undefined)
+            throw new Error("Device information is undefined. This might mean it hasn't been initialized yet.");
+        return this._deviceInformation;
+    }
+
+    public get uaDataValues() {
+        return this._uaDataValues;
+    }
+
+    public get uaParser() {
+        return this._uaParser;
+    }
+
+    /** Constructs the parser and returns it.  */
+    public static initialize(): DetailedDeviceInformationProvider {
+        // Assign essentially an alias for readability.
+        const DDIP = DetailedDeviceInformationProvider;
+        const navigatorUAData: NavigatorUAData | undefined = navigator?.userAgentData;
+        const uaParser = new UAParser(navigator.userAgent);
+        let deviceInformation: DetailedDeviceInformation | undefined;
+
+        // User-Agent Client Hints API is not supported, use parser exclusively instead.
+        if (!navigatorUAData) {
+            deviceInformation = {
+                deviceModel: DDIP.getDeviceModel(uaParser) ?? 'Device model not found.',
+                operatingSystem: DDIP.getOperatingSystem(uaParser) ?? 'Operating system not found.',
+                webBrowser: DDIP.getWebBrowser(uaParser) ?? 'Web browser not found',
+                platform: DDIP.getPlatform(uaParser)
+            };
+
+            // Call constructor and return the thin-air object.
+            return new DDIP({
+                deviceInformation: deviceInformation,
+                uaDataValues: navigatorUAData,
+                uaParser: uaParser
+            });
+        }
+
+        // User-Agent Client Hints API is supported and initialized, use it with fallbacks to US parsing if we get falseys to specific props.
+        let webBrowser = DDIP.getWebBrowser(navigatorUAData);
+        if (webBrowser.length === 0) webBrowser = DDIP.getWebBrowser(uaParser);
+
+        let operatingSystem = DDIP.getOperatingSystem(navigatorUAData);
+        if (operatingSystem.length === 0) operatingSystem = DDIP.getOperatingSystem(uaParser);
+
+        const deviceModel = DDIP.getDeviceModel(uaParser);
+
+        let platform = DDIP.getPlatform(navigatorUAData);
+        if (!platform) platform = DDIP.getPlatform(uaParser);
+
+        deviceInformation = {
+            webBrowser: webBrowser || 'Web browser not found.',
+            operatingSystem: operatingSystem || 'Operating system not found.',
+            deviceModel: deviceModel || 'Device model not found.',
+            platform: platform || 'Platform not found.'
+        } as DetailedDeviceInformation;
+
+        // Call constructor and return the thin-air object.
+        return new DDIP({
+            deviceInformation: deviceInformation,
+            uaDataValues: navigatorUAData,
+            uaParser: uaParser
+        });
+    }
+
+    private static getPlatform(dataOrigin: UAParser | UALowEntropyJSON): SupportedPlatforms | undefined {
+        if (dataOrigin instanceof UAParser) {
+            const platform = dataOrigin.getBrowser().name;
+            if (platform && isSupportedPlatform(platform)) return platform;
+            else return undefined;
+        }
+
+        const platform = dataOrigin.platform;
+        if (platform && isSupportedPlatform(platform)) {
+            return platform;
+        }
+
+        return undefined;
+
+        function isSupportedPlatform(target: string): target is SupportedPlatforms {
+            switch (target.toLowerCase()) {
+                case 'android':
+                case 'linux':
+                case 'macos':
+                case 'ios':
+                case 'windows':
+                case 'ipados':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+
+    private static getWebBrowser(dataOrigin: UAParser | UALowEntropyJSON): string {
+        if (dataOrigin instanceof UAParser) {
+            const { name, version } = dataOrigin.getBrowser();
+            if (!name && !version) return '';
+            return `${name} ${version}`;
+        }
+
+        const browsers = dataOrigin.brands;
+        if (browsers?.length === 0) return '';
+        let browserNameVersion = '';
+
+        browsers?.forEach((browser) => (browserNameVersion += `${browser.brand} ${browser.version};`));
+
+        return browserNameVersion;
+    }
+
+    private static getOperatingSystem(dataOrigin: UAParser | UALowEntropyJSON): string {
+        if (dataOrigin instanceof UAParser) {
+            const { name: osName, version: osVersion } = dataOrigin.getOS();
+            if (!osName && !osVersion) return '';
+            return `${osName} ${osVersion}`;
+        }
+
+        const OS = dataOrigin.platform;
+        if (!OS) return '';
+        return OS;
+    }
+
+    /**
+     * TODO: Get the device model from Client Hints API.
+     * This requires top-level await support, which in turn has to be implemented to modules downstream.
+     */
+    private static getDeviceModel(dataOrigin: UAParser): string {
+        const { model, vendor } = dataOrigin.getDevice();
+        if (!model && !vendor) return '';
+        return `${model} ${vendor}`;
+    }
+}
+
+const detailedDeviceInformationProvider = DetailedDeviceInformationProvider.initialize();
+export { DetailedDeviceInformationProvider, detailedDeviceInformationProvider };
---
+++ src/utils/deviceInfo/deviceInfo.ts
@@ -0,0 +1,172 @@
+import {
+    Browser,
+    DetailedDeviceInformation,
+    DeviceType,
+    IDeviceSummary,
+    ScreenDimensions,
+    ScreenOrientation,
+    UserInput
+} from '../../types/device';
+import { detailedDeviceInformationProvider } from './deepDeviceInfo';
+
+type Props = { detailedDeviceInfo: typeof detailedDeviceInformationProvider };
+
+class DeviceInformation {
+    /**
+     * Represents the current viewport orientation.
+     * If the viewport is square, it will default to portrait.
+     */
+    private _orientation: ScreenOrientation;
+
+    /**
+     * Represents an abstracted device type: desktop, tablet or mobile.
+     */
+    private readonly _deviceType: DeviceType;
+
+    /**
+     * Represents the current input source.
+     * Note: On a touchscreen device which has a mouse plugged in, it will return the mouse.
+     */
+    private readonly _userInput: UserInput;
+
+    private readonly _browser?: Browser;
+
+    /**
+     * Represents the viewport (layout and visual) and (full)screen dimensions.
+     * WebKit browsers prior to 15 will only support layout dimensions.
+     */
+    private _dimensions: ScreenDimensions;
+
+    // MediaQueryLists
+    protected orientationIsLandscapeQuery: MediaQueryList;
+
+    private _detailedDeviceInfo: typeof detailedDeviceInformationProvider;
+
+    constructor(props: Props) {
+        this._detailedDeviceInfo = props.detailedDeviceInfo;
+        this.orientationIsLandscapeQuery = window.matchMedia('(orientation: landscape)');
+
+        this._userInput = this.getUserInput();
+        this._deviceType = this.getDeviceType();
+        this._dimensions = this.getUpdatedScreenDimensions();
+        this._orientation = this.getOrientation();
+        this._browser = this.getBrowser();
+
+        // TODO: Once ScreenOrientation API is supported on Safari, this event can be removed.
+        this.orientationIsLandscapeQuery.addEventListener('change', () => (this._orientation = this.getOrientation()));
+    }
+
+    private getUpdatedScreenDimensions(): ScreenDimensions {
+        const updatedDimensions = {
+            layout: {
+                width: window.innerWidth,
+                height: window.innerHeight
+            },
+            visual: window.visualViewport
+                ? {
+                      width: window.visualViewport.width,
+                      height: window.visualViewport?.height
+                  }
+                : undefined,
+            fullscreen: window.screen
+                ? {
+                      available: {
+                          width: window.screen.availWidth,
+                          height: window.screen.availHeight
+                      },
+                      total: {
+                          width: window.screen.width,
+                          height: window.screen.height
+                      }
+                  }
+                : undefined
+        } as ScreenDimensions;
+        this._dimensions = updatedDimensions;
+        return updatedDimensions;
+    }
+
+    private getBrowser(): Browser | undefined {
+        const browser = this._detailedDeviceInfo.deviceInformation.webBrowser.toLowerCase();
+
+        if (browser.includes('chrome') || browser.includes('chromium')) return 'Chrome';
+        if (browser.includes('firefox')) return 'Firefox';
+        if (browser.includes('opera')) return 'Opera';
+        if (browser.includes('safari')) return 'Safari';
+        if (browser.includes('edge')) return 'Edge';
+
+        return undefined;
+    }
+
+    private getUserInput() {
+        const usingTouchQuery = window.matchMedia('(pointer: coarse)');
+        return usingTouchQuery.matches ? 'touch' : 'mouse';
+    }
+
+    /** Returns the current viewing medium.*/
+    private getDeviceType(): DeviceType {
+        const deviceInfo = this._detailedDeviceInfo.deviceInformation;
+
+        // Find iDevices
+        if (deviceInfo.deviceModel === 'iPad Apple') return 'tablet';
+        if (deviceInfo.deviceModel === 'iPhone Apple') return 'mobile';
+        if (deviceInfo.platform === 'MacOs') return 'desktop';
+
+        // Find Android phones.
+        if (deviceInfo.platform === 'Android') return 'mobile';
+
+        /* Find Windows desktops.
+         * Implementers should note users will have touch screens on Windows desktop devices. */
+        if (deviceInfo.platform === 'Windows') return 'desktop';
+
+        /* Find Linux desktops with mouse inputs.
+         * A clash with Android mobile or tablet is a possibility. */
+        if (deviceInfo.platform === 'Linux' && this._userInput === 'mouse') return 'desktop';
+
+        return 'desktop';
+    }
+
+    private getOrientation(): ScreenOrientation {
+        if (this.orientationIsLandscapeQuery.matches) return 'landscape';
+        else return 'portrait';
+    }
+
+    public getDeviceDetails(): DetailedDeviceInformation {
+        return this._detailedDeviceInfo.deviceInformation;
+    }
+
+    public get orientation() {
+        return this._orientation;
+    }
+
+    public get deviceType() {
+        return this.getDeviceType();
+    }
+
+    public get userInput() {
+        return this._userInput;
+    }
+
+    public get screenDimensions(): ScreenDimensions {
+        return this.getUpdatedScreenDimensions();
+    }
+
+    public get browser(): Browser | undefined {
+        return this._browser;
+    }
+
+    public getDeviceSummary(): IDeviceSummary {
+        return {
+            orientation: this.orientation,
+            deviceType: this.deviceType,
+            userInput: this.userInput,
+            screenDimensions: this.screenDimensions,
+            deepInfo: this.getDeviceDetails()
+        };
+    }
+}
+
+const deviceInfo = new DeviceInformation({
+    detailedDeviceInfo: detailedDeviceInformationProvider
+});
+
+export { deviceInfo, DeviceInformation };
